877288b0105c15a9196f0daff97557f1fe2d6861
Laradji Nacer <nacer.laradji@gmail.com>
2012-07-11 14:59:50 +0200

    graphite url

--- attributes/gdash.rb
+++ attributes/gdash.rb
@@ -7,7 +7,7 @@ default.gdash.url = "https://github.com/ripienaar/gdash/tarball/master"
 # TODO: put this in a databag ?
 ####
 include_attribute "graphite::graphite"
-default.gdash.graphite_url = "http://#{graphite[:url]}"
+default.gdash.graphite_url = "graphite"
 ####
 
 default.gdash.templatedir = "/srv/gdash/graph_templates"
